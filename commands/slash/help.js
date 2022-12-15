6b6b90d0218ce90b132d54743274ee1ab7065436
KristianCorrea <kristiangcorrea@live.com>
2022-12-15 15:23:39 -0500

    Added env variables

--- commands/slash/help.js
+++ commands/slash/help.js
@@ -121,7 +121,7 @@ const command = new SlashCommand()
 					require("../../package.json").version
 				}; Build: ${ gitHash }` +
 				"\n" +
-				`[✨ Support Server](${ client.config.supportServer }) | [Issues](${ client.config.Issues }) | [Source](https://github.com/SudhanPlayz/Discord-MusicBot/tree/v5) | [Invite Me](https://discord.com/oauth2/authorize?client_id=${ client.config.clientId }&permissions=${ client.config.permissions }&scope=bot%20applications.commands)`,
+				"Chimmi Chonga",
 			});
 			await iter.update({
 				embeds: [helpEmbed],
